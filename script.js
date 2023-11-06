1cd8898bfb00d6bbe7ebf46bcfedf5b246872f77
Kid-Developer <149759921+kid-Deverloper@users.noreply.github.com>
2023-11-06 21:37:02 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredslides: true,
+    loop:true,
+    autoplay: {
+        delay:3000,
+        disableOnInteraction: false,
+    },
+    
+});
