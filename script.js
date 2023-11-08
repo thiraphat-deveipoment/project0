c3b5e2309dcfca1078f80dce7ce05b1693a7781f
Kid-Developer <149759921+kid-Deverloper@users.noreply.github.com>
2023-11-08 13:57:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,24 @@ var swiper = new Swiper(".mySwiper", {
     },
     
 });
+
+/*=== navbar toggle ===*/
+let nacBar = document.querySelector('.navlink');
+let  menuBar = document.querySelector('.#menuBtn');
+
+menuBar.onclick = ()=> {
+    navBar.classList.toggle('.active');
+    menuBar.classList.toggle('.bx-x');
+}
+/*=== Product Section start ===*/
+var swiper = new swiper(".myproduct",{
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop:true,
+    breakpoints: {
+        550: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        }
+    }
+});
